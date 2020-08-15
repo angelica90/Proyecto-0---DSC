03b51faa14b016add232b36b64219b0970b4d3ad
angelica90 <51386039+angelica90@users.noreply.github.com>
2020-08-14 21:32:42 -0500

    Update models.py

--- api/core/models.py
+++ api/core/models.py
@@ -14,14 +14,14 @@ def __str__(self):
 
 class create_event(models.Model):
     event_name = models.CharField(max_length=50)
-    CATEGORIAS = (('CONFERENCE', 'Conferencia'), ('SEMINAR', 'Seminario'), ('CONGRESS', 'Curso'), ('COURSE', 'Congreso'))
-    event_category = models.CharField(max_length=10, choices=CATEGORIAS, null=False, blank=False)
+    category_events = (('CONFERENCE', 'Conferencia'), ('SEMINAR', 'Seminario'), ('CONGRESS', 'Curso'), ('COURSE', 'Congreso'))
+    event_category = models.CharField(max_length=10, choices=category_events, null=False, blank=False)
     event_place = models.CharField(max_length=50)
     event_address = models.CharField(max_length=50)
     event_initial_date = models.DateTimeField()
     event_final_date = models.DateTimeField()
-    TIPOS = (('PRESENCIAL', 'Presencial'), ('VIRTUAL', 'Virtual'))
-    event_type = models.CharField(max_length=10, choices=TIPOS, null=False, blank=False)
+    types_events = (('PRESENCIAL', 'Presencial'), ('VIRTUAL', 'Virtual'))
+    event_type = models.CharField(max_length=10, choices=types_events, null=False, blank=False)
     event_creation_date = models.DateTimeField(auto_now_add=True)
     event_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     thumbnail = models.ImageField("Imagen", upload_to="recipe_thumbnails", default="recipe_thumbnails/default.png")
